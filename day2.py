7efc391959814c1718ef4a67bf63860b92c81c3d
Kyle Sun <kyle@bigkyle.com>
2017-12-05 23:18:35 -0800

    day 2 part 2

--- day2.py
+++ day2.py
@@ -6,5 +6,23 @@ def main():
             ret += max(line) - min(line)
     print(ret)
 
+
+def main2():
+    ret = 0
+    with open("day2.txt") as ifp:
+        for row in ifp:
+            line = [int(x) for x in row.split("\t")]
+            found = False
+            for i, x in enumerate(line):
+                for j, y in enumerate(line):
+                    if i != j and x % y == 0:
+                        ret += x//y
+                        found = True
+                        break
+                if found:
+                    break
+    print(ret)
+
 if __name__ == '__main__':
     main()
+    main2()
